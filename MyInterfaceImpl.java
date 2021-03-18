460d12b671e52492888c6f279ba13f008a2a2a4d
Ling Ye <lingye0926@gmail.com>
2021-03-18 17:48:54 +0900

    delete

--- MyInterfaceImpl.java
+++
@@ -1,8 +0,0 @@
-package cn.itcast.day11.demo05;
-
-public class MyInterfaceImpl implements MyInterface{
-    @Override
-    public void method() {
-        System.out.println("实现类覆盖重写了方法！");
-    }
-}
